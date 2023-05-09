9fc0e90b9fa6134c48594d71ca0d4d6e2158fbc8
manishsingh8 <manishsingh04031998@gmail.com>
2023-05-09 13:10:39 +0530

    first commit

---
+++ src/pages/explore/Explore.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.scss';
+import Img from '../../components/lazyLoadImage/Img';
+const Explore = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Explore
